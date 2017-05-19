7f2c74e65f1a54ada9717948c4158107370913ee
Aleksandr S Melnik <alexmelnik224@gmail.com>
2017-05-19 12:13:59 -0700

    Add files via upload

---
+++ letterprint.c
@@ -0,0 +1,15 @@
+#include <stdio.h> /* Needed for the print function */
+
+ 
+
+void main(void)
+
+{
+
+char a='b'; /* Sets the variable a to equal character b Note: I am using b to highlight how this will work */
+
+printf("%c", a); /* This should print the character b */
+
+printf("\n"); /* Prints a blank line, this is not needed but will make this program easier to read once compiled */
+
+}
