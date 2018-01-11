f29ccaea7d821f5018076000b1b86b445113dfb4
Aleksandr S Melnik <alexmelnik224@gmail.com>
2018-01-11 12:44:24 -0800

    Add files via upload

---
+++ ccode/hello2.c
@@ -0,0 +1,11 @@
+// save this file as hello.c 
+// compile $gcc hello.c -o hello.o
+// tp rime $./hello.o
+
+#include <stdio.h>
+
+	int main(){
+	printf ("hellow world");
+
+	return 0;
+}
---
+++ ccode/helloworld.c
@@ -0,0 +1,13 @@
+//helloworld.c
+//save file as helloworld.c
+//complie $gcc helloworld.c -o helloworld.o
+//run ./helloworld.o
+
+#include <stdio.h>
+
+int main(){
+	
+	printf ("hello world \n\n");
+	
+}
+
